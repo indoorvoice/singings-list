df9c8604eace91d533a50555ed70f6f119f0abcf
Mike Morrisroe <morrisroem@DUB-MAC-MORRIS.local>
2018-03-30 17:00:03 +0100

    init commit. Set up basic scaffold with vue-cli3 and added a few basic components.

--- src/main.js
+++ src/main.js
@@ -6,6 +6,19 @@ import "./registerServiceWorker";
 
 Vue.config.productionTip = false;
 
+
+// bootstrap vue setup
+import BootstrapVue from "bootstrap-vue";
+import vSelect from 'vue-select';
+
+Vue.use(BootstrapVue);
+
+Vue.component('v-select', vSelect);
+
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap-vue/dist/bootstrap-vue.css';
+
+
 new Vue({
   router,
   store,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,72 @@
 <template>
-  <div class="home">
-    <img src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="home wrapper">
+    <div class="sidebar">
+      <datepicker placeholder="Start Date"></datepicker>
+      
+      <datepicker placeholder="End Date"></datepicker>
+
+      <v-select :options="books"></v-select>
+      
+      <v-select :options="countries"></v-select>
+      
+    </div>
+
+    <div class="header">
+      <h1>Singings!</h1>
+    </div>
+
+    <div class="list-holder">
+      <p v-for="i in 10" :key="i">{{ i*i }}</p>
+    </div>
   </div>
 </template>
+
+<style>
+.wrapper {
+  display: grid;
+  grid-template-columns: 1fr 4fr;
+  grid-template-rows: 1fr 3fr;
+}
+
+.sidebar {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  grid-row-start: 1;
+  grid-row-end: 5;
+  background-color: lightpink;
+}
+
+.header {
+  grid-column-start: 2;
+  grid-column-end: 4;
+  grid-row-start: 1;
+  grid-row-end: 2;
+  background-color: lightblue;
+}
+
+.list-holder {
+  grid-column-start: 2;
+  grid-column-end: 4;
+  grid-row-start: 2;
+  grid-row-end: 5;
+  background-color: lightgoldenrodyellow;
+}
+</style>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import Datepicker from 'vuejs-datepicker';
+
 
 export default {
   name: "home",
+  data: () => ({
+    books: ['Denson Sacred Harp', 'Shenandoah Harmony', 'Christian Harmony', 'Cooper Sacred Harp'],
+    bookSelected: '',
+  }),
   components: {
-    HelloWorld
+    Datepicker,
   }
 };
 </script>
